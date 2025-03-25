ff499c8665690b7b59fc6615cb41276d0268e309
珊 <Wangshan9102@163.com>
2025-03-26 04:46:46 +0800

    2025-03-26 04:46:43

--- Assets/Scripts/TimerTrigger.cs
+++ Assets/Scripts/TimerTrigger.cs
@@ -1,18 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TimerTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // 计时器
+    private float timer = 0f;
+    // 最大时间（100秒）
+    public float maxTime = 100f;
 
-    // Update is called once per frame
+    // 定义Unity事件列表
+    public UnityEvent onTimerComplete;
+
     void Update()
     {
-        
+        // 计时器加速
+        if (timer < maxTime)
+        {
+            timer += Time.deltaTime;
+        }
+
+        // 如果计时器达到最大时间，触发事件
+        if (timer >= maxTime)
+        {
+            if (onTimerComplete != null)
+            {
+                onTimerComplete.Invoke();
+            }
+            // 重置计时器，或者您可以选择销毁脚本或其他操作
+            timer = 0f;
+        }
     }
 }
